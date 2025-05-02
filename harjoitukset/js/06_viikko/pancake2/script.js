7732bc747398a7decd31d7ebe743a11cbb6c7555
iflix0fox <ahmadalazzawi5080@gmail.com>
2025-05-02 12:47:30 +0300

    oloit

--- harjoitukset/js/06_viikko/pancake2/script.js
+++ harjoitukset/js/06_viikko/pancake2/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const totalPriceBanner = document.getElementById("totalPrice");
   const totalPriceDisplay = document.getElementById("totalPriceDisplay");
   const orderButton = document.getElementById("seeOrder");
-  const summryText = document.getElementById("summaryText");
+  const summaryText = document.getElementById("summaryText");
 
   let toppings = [];
   let extras = [];
@@ -42,10 +42,37 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function updatePrice() {
     const pancakeType = document.getElementById("type");
-    const selectedType = pancakeType.option[pancakeType.selectedIndex];
+    const selectedType = pancakeType.options[pancakeType.selectedIndex];
     let total = parseFloat(selectedType.getAttribute("data-price"));
 
-    totalPriceBanner.textContent = total;
-    totalPriceDisplay.textContent = total;
+    total = total + toppings.length * 1;
+    // haetaan lisukkeet listaan ja ksäitellään lista. Lisätään valittujan hinta
+    let extraChoises = document.querySelectorAll(".extra");
+    extraChoises.forEach((checkbox) => {
+      {
+        if (checkbox.checked) {
+          total = total + parseFloat(checkbox.getAttribute("data-price"));
+        }
+      }
+    });
+
+    // haetaan klujetuksen arvo ja lisätään hinta (hinta voi olla nolla)
+    let delivery = document.querySelector("input[name='delivery']:checked");
+    total += parseFloat(delivery.getAttribute("data-price"));
+
+    // motoillaan kokonaishinta desimaalisksi
+    let formatedTotal = total.toFixed(2) + " €";
+    totalPriceBanner.textContent = formatedTotal;
+    totalPriceDisplay.textContent = formatedTotal;
   }
+  orderButton.addEventListener("click", function () {
+    const customerName = document.getElementById("customerName").value.trim();
+
+    let summary = `<strong>Asiaks:</strong> ${
+      customerName || "(ei nimeä)"
+    }<br>`;
+    summary += "Täytteet:" + selectedType + "<br>;";
+
+    summaryText.innerHTML = summary;
+  });
 });
