95796e18477b222ec8c537929510c291b2010247
iflix0fox <ahmadalazzawi5080@gmail.com>
2025-05-24 03:49:36 +0300

    update portofolio, contact, galleria

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,1 +1,11 @@
 console.log("Hello, world! Welcome to my static website.");
+
+//const flipContainer = document.querySelector(".flip-container");
+
+// flipContainer.addEventListener("mouseenter", () => {
+//   flipContainer.style.backgroundColor = " black "; // color when flipped
+// });
+
+// flipContainer.addEventListener("mouseleave", () => {
+//   flipContainer.style.backgroundColor = ""; // reset to default
+// });
